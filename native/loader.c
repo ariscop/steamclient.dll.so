b650e7500d30a1bf0d1fdebd189ed85ca08ae9bc
Andrew Cook <ariscop@gmail.com>
2013-07-06 15:05:33 +1000

    derp

--- native/loader.c
+++ native/loader.c
@@ -8,7 +8,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(steamclient);
 void *steamclient_so;
 
 int Steamclient_load() {
-    steamclient_so = dlopen("/home/andrew/.steam/steam/ubuntu_32/steamclient.so", RTLD_LOCAL | RTLD_LAZY);
+    steamclient_so = dlopen("/home/andrew/.steam/steam/ubuntu12_32/steamclient.so", RTLD_LOCAL | RTLD_LAZY);
     WINE_TRACE("steamclient.so at %p\n", steamclient_so);
     
     return steamclient_so != 0;
